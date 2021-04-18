bed377ba4bd9c5aebe9f16571963cf2a87373124
ruei <ruei@null.computer>
2021-04-18 16:24:17 +0800

    :sparkles: add comment for 1588

--- 1588-Sum-of-All-Odd-Length-Subarrays/index.js
+++ 1588-Sum-of-All-Odd-Length-Subarrays/index.js
@@ -1,3 +1,13 @@
+/* 
+方法：
+第一個迴圈用來計算當前的奇數是多少，所以每次會加2
+第二個迴圈產生兩個指針，start表示起始位置，end為結束位置，結束位置會依照當前的奇數而變動
+例如： 第一層迴圈為 3 時，表示現在在找長度為三的subarray，所以start ～ end 的位移量是3，而start從0開始，所以end會是2
+第三個迴圈則是透過迴圈2產生的slider window 內，所有數值的加總。
+需注意的是第二個迴圈的限制條件，slider window 的結束位置不可以超出array長度範位
+
+time: O(n^3), space: O(1)
+*/
 const sumOddLengthSubarrays = function(arr) {
   let len = 1;
   let sum = 0;
